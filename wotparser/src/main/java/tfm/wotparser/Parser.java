119023dbf6fcee8f2bb22dd7b9e844cb1eda870b
juanfran.tiempo@gmail.com <juanfran@tiempo.com>
2021-07-01 11:12:06 +0200

    Fix ulstrasounf and PARSER

--- wotparser/src/main/java/tfm/wotparser/Parser.java
+++ wotparser/src/main/java/tfm/wotparser/Parser.java
@@ -17,12 +17,22 @@ public class Parser {
 		return "import " + lib + ";\n";
 	}
 	
-	public static String getTimerConfiguration(int time) {
+	public static String getTimerConfiguration(int time, int caso) {
+		if(caso==5) {
+			return "task:Scheduler timer = new({\n"
+					+ "intervalInMillis: "+time+",\n"
+					+ "initialDelayInMillis: 0\n"
+					+ "});\n"
+					+ "var attachResult = timer.attach(service1);\n"
+					+ "var startResult = timer.start();\n";
+
+		}else {
 		return "task:TimerConfiguration timerConfiguration = {\n"
 				+ "intervalInMillis: "+time+",\n"
 				+ "initialDelayInMillis: 0\n"
 				+ "};\n"
 				+ "listener task:Listener timer = new(timerConfiguration);\n";
+		}
 	}
 	
 	public static String newEndpoint(String name, String host) {
@@ -35,7 +45,9 @@ public class Parser {
 		for (String s: endpoints) {  
 			String[] d = s.split("\\s+");
 			String c_host = d[1];
-			salida += Parser.newEndpoint(base+j, c_host);
+			String d_type = d[0];
+			if(!d_type.equals("mqtt"))
+				salida += Parser.newEndpoint(base+j, c_host);
 		    j++;
 		}
 
@@ -90,7 +102,7 @@ public class Parser {
 		return salida;
 	}
 	
-	public static String getCallback(String[] antecedentes, String[] consecuentes, String condicion) {
+	public static String getCallback(String[] antecedentes, String[] consecuentes, String condicion, int caso) {
 		String salida = "";
 		int lastNumberCondition = antecedentes.length;
 		ArrayList<String> condicionesAll = new ArrayList<String>();
@@ -102,7 +114,7 @@ public class Parser {
 		
 		int i=1;
 		
-		if(antecedentes.length>1) { //Varios MQTT
+		if(antecedentes.length>1 || caso==5) { //Varios MQTT
 			for (String s: antecedentes) {  
 				String[] d = s.split("\\s+");
 				String d_topic = d[2];
@@ -121,8 +133,10 @@ public class Parser {
 			
 			String condicion_bal = condicion;
 
-			for(int k = 1; k <= lastNumberCondition;k++) {
-				condicion_bal = condicion_bal.replace(""+k+"", "fired"+k);
+			if(caso!=5) {
+				for(int k = 1; k <= lastNumberCondition;k++) {
+					condicion_bal = condicion_bal.replace(""+k+"", "fired"+k);
+				}
 			}
 			salida += ("\n\t\tif("+condicion_bal+"){\n");
 
@@ -149,42 +163,93 @@ public class Parser {
 		return salida;
 	}
 	
-	public static String newService(String[] antecedentes, String[] consecuentes, String condicion, int lastNumberCondition) {
+	public static String newService(String[] antecedentes, String[] consecuentes, String condicion, int lastNumberCondition, int caso) {
 		    
 		ArrayList<String> condicionesAll = new ArrayList<String>();
 
-		String salida = "service timerService on timer {\n"
-				+ "    resource function onTrigger() {\n";
+		String salida = "";
+
+		if(caso==5) {
+			salida += "service service1 = service {\n";
+		}else {
+			salida += "service timerService on timer {\n";
+		}
+		salida += "\tresource function onTrigger() {\n";
 		
 		int i=1;
 		for (String s: antecedentes) {  
 			String[] d = s.split("\\s+");
 			String d_condition = d[2];
+			String d_type = d[0];
 			condicionesAll.add(d_condition);
-			salida += "\t\tvar res"+i+" = antecedente"+i+"->get(\"\");\n";
+			if(d_type.equals("get")) {
+				salida += "\t\tvar res"+i+" = antecedente"+i+"->get(\"\");\n";
+			}
 			i++;
+
 		}
 			salida += "\n\t\tif(";
 			
-  		for (int j=1; j<=antecedentes.length;j++) {  
-			if(j==1)
-    			salida += "res"+j+" is http:Response";
-			else
-    			salida += " && res"+j+" is http:Response";
-		}
-		salida += ") {\n";
+			
+  		if(caso==5) {
+  	  		i=1;
+    		boolean escribo = false;
+      		for (String s: antecedentes) {  
+    			String[] d = s.split("\\s+");
+    			String d_type = d[0];
+    			if(d_type.equals("get")) {
+	    			if(escribo==false) {
+	    				salida +=("res"+i+" is http:Response");
+		    			escribo=true;
+	    			}else
+	    				salida +=(" && res"+i+" is http:Response");
+    			}
+    			i++;
+			}
+  		}else {
+  	  		for (int j=1; j<=antecedentes.length;j++) {  
+  				if(j==1)
+  	    			salida += "res"+j+" is http:Response";
+  				else
+  	    			salida += " && res"+j+" is http:Response";
+  			}
+  		}
 
+		salida += ") {\n";
   		i=1;
-		for (int k=1; k<=antecedentes.length;k++) {  
-			salida += "\t\t\tjson jsonpayload"+k+" = checkpanic res"+k+".getJsonPayload();\n"+
+  		
+  		if(caso==5) {
+    		for (String s: antecedentes) {  
+    			String[] d = s.split("\\s+");
+    			String d_type = d[0];
+
+    			if(d_type.equals("get")) {
+    				salida +=("\t\t\tjson jsonpayload"+i+" = checkpanic res"+i+".getJsonPayload();\n"+
+	    						"\t\t\tvar condition"+i+" = <float>jsonpayload"+i+"."+condicionesAll.get(i-1)+";\n");
+    				salida +=("            if(condition"+i+"){\r\n"
+    						+ "                firedGet"+i+" = true;\r\n"
+    						+ "            }else{\r\n"
+    						+ "                firedGet"+i+" = false;\r\n"
+    						+ "            }\r\n");
+    			}
+    			i++;
+			}
+
+  		}else {
+  			for (int k=1; k<=antecedentes.length;k++) {  
+  				salida += "\t\t\tjson jsonpayload"+k+" = checkpanic res"+k+".getJsonPayload();\n"+
 						"\t\t\tvar condition"+k+" = <float>jsonpayload"+k+"."+condicionesAll.get(k-1)+";\n";
-		}
+  			}
+  		}
 		
+
 		String condicion_bal = (condicion!=null) ? condicion : "1";
 
-		for(int l = 1; l <= lastNumberCondition;l++) {
-			condicion_bal = condicion_bal.replace(""+l+"", "condition"+l);
-		}
+  		if(caso!=5) {
+			for(int l = 1; l <= lastNumberCondition;l++) {
+				condicion_bal = condicion_bal.replace(""+l+"", "condition"+l);
+			}
+  		}
 		salida += "\t\t\tif("+condicion_bal+"){\n";
 		
 		//SALIDAS
@@ -197,17 +262,24 @@ public class Parser {
 				salida += "\t\t\t\tvar response"+i+" = consecuente"+i+"->put(\"\","+d_payload+");\r\n";
 			i++;
 		}
-		salida += "\t\t\t}\n"
-				+ "\t\t}\n"
-				+ "\t}\n"
-				+ "}\n";
-		
+
+		if(caso==5) {
+			salida += "\t\t\t}\n"
+					+ "\t\t}\n"
+					+ "\t}\n"
+					+ "};\n";
+		}else {
+			salida += "\t\t\t}\n"
+					+ "\t\t}\n"
+					+ "\t}\n"
+					+ "}\n";
+		}
+
 		return salida;
 	}
 	
 	public static void parseFile(MultipartFile file) throws IOException {
 		// TODO Auto-generated method stub
-			//String file ="C:\\Users\\Juanfran\\Desktop\\TFM\\ejemplo.txt";
 		    String fileOutput ="output/salida.bal";
 		    BufferedReader reader = new BufferedReader(new InputStreamReader(file.getInputStream()));
 		    BufferedWriter writer = new BufferedWriter(new FileWriter(fileOutput));
@@ -224,7 +296,6 @@ public class Parser {
     		if(lines==3)
     			condicion = reader.readLine();
 
-    		//int lastNumberCondition = Integer.parseInt(condicion.split("\\s+")[condicion.split("\\s+").length-1].replace("(", "").replace(")", ""));
 		    int lastNumberCondition = antecedentesAll.length;
 
 		    String consecuentes = reader.readLine();
@@ -241,9 +312,9 @@ public class Parser {
 		    		writer.write(Parser.getImport("ballerina/task"));
 		    		writer.write(Parser.newsEndpoints(antecedentesAll, "antecedente"));
 		    		writer.write(Parser.newsEndpoints(consecuentesAll, "consecuente"));
-		      		writer.write(Parser.getTimerConfiguration(Integer.parseInt(antecedentes.split(" ")[3])));
+		      		writer.write(Parser.getTimerConfiguration(Integer.parseInt(antecedentes.split(" ")[3]),3));
 		      		writer.write(Parser.getMain(null, 1));
-		    		writer.write(Parser.newService(antecedentesAll, consecuentesAll, condicion, lastNumberCondition));
+		    		writer.write(Parser.newService(antecedentesAll, consecuentesAll, condicion, lastNumberCondition,3));
 
 		    	} else if (!antecedentes.contains("get")){ //Varios MQTT
 		    		
@@ -251,100 +322,17 @@ public class Parser {
 		    		writer.write(Parser.getImport("ballerina/runtime"));
 		    		writer.write(Parser.getImport("ballerina/lang.'string as str"));
 
-//		    	    writer.write("import pzfreo/mqtt;\n"
-//		    	    		+ "import ballerina/runtime;\n"
-//		    	    		+ "import ballerina/lang.'string as str;\n");
 
 		    		writer.write(Parser.newsEndpoints(consecuentesAll, "consecuente"));
 
-//		    		int j = 1;
-//		    		for (String s: consecuentesAll) {  
-//		    			String[] d = s.split("\\s+");
-//		    			String d_method = d[0];
-//		    			String d_host = d[1];
-//		    			String d_payload = d[2];
-//		    			writer.write("http:Client clientEndpoint"+j+" = new (\""+d_host+"\");\r\n");
-//		    		    j++;
-//		    		}
 		    		
 		    		writer.write(Parser.getNewsBooleans(antecedentesAll, "fired"));
 
-//					int i=1;
-//		    		for (String s: antecedentesAll) {  
-//		    			String[] d = s.split("\\s+");
-//		    			String d_host = d[1];
-//		    			String d_condition = d[3];
-//		    			writer.write("boolean fired"+i+" = false;\r\n");
-//		    			i++;
-//					}
 
 			       	writer.write(Parser.getMain(antecedentesAll, 0));		    		
 
-			    	writer.write(Parser.getCallback(antecedentesAll, consecuentesAll, condicion));
+			    	writer.write(Parser.getCallback(antecedentesAll, consecuentesAll, condicion, 4));
 
-//	    			writer.write("\npublic function main() returns error? {\n");
-//
-//					ArrayList<String> condicionesAll = new ArrayList<String>();
-//					i=1;
-//		    		for (String s: antecedentesAll) {  
-//		    			String[] d = s.split("\\s+");
-//		    			String d_host = d[1];
-//		    			String d_topic = d[2];
-//		    			String d_condition = d[3];
-//		    			condicionesAll.add(d_condition);
-//		    			writer.write("\tmqtt:Client c"+i+" = check new(\"tcp://"+d_host+"\", \"\");\r\n"
-//		    					+ "    check c"+i+"->connect();\r\n"
-//		    					+ "    Callback cb"+i+" = new;\r\n"
-//		    					+ "    check c"+i+"->subscribe(\""+d_topic+"\", cb"+i+");\r\n\n");
-//		    			i++;
-//					}
-//	    			writer.write("\twhile(true){\r\n"
-//	    					+ "        runtime:sleep(1000);\r\n"
-//	    					+ "    }\r\n"
-//	    					+ "    check c1->disconnect();\r\n"
-//	    					+ "}\n\n"
-//	    					+ "public type Callback object {\r\n"
-//	    					+ "    public function onMessage(mqtt:Message m)  {\n"
-//	    					+ "        var payload = checkpanic str:fromBytes(m.payload);\r\n"
-//	    					+ "        json jsonpayload = checkpanic payload.fromJsonString();\n");
-//
-//	    			i=1;
-//		    		for (String s: antecedentesAll) {  
-//		    			String[] d = s.split("\\s+");
-//		    			String d_host = d[1];
-//		    			String d_topic = d[2];
-//		    			String d_condition = d[3];
-//		    			condicionesAll.add(d_condition);
-//		    			writer.write("\t\tif(m.topic == \""+d_topic+"\"){\r\n"
-//		    					+ "            if(jsonpayload."+d_condition+"){\r\n"
-//		    					+ "                fired"+i+" = true;\r\n"
-//		    					+ "            }else{\r\n"
-//		    					+ "                fired"+i+" = false;\r\n"
-//		    					+ "            }\r\n"
-//		    					+ "        }\n");
-//		    			i++;
-//					}
-//		    		
-//		    		String condicion_bal = condicion;
-//
-//		    		for(int k = 1; k <= lastNumberCondition;k++) {
-//		    			condicion_bal = condicion_bal.replace(""+k+"", "fired"+k);
-//		    		}
-//	    			writer.write("\n\t\tif("+condicion_bal+"){\n");
-//		    		
-//		    		//SALIDAS
-//	    			i=1;
-//		    		for (String s: consecuentesAll) {  
-//		    			String[] d = s.split("\\s+");
-//		    			String d_method = d[0];
-//		    			String d_payload = d[2];
-//		    			if(d_method.equals("post"))
-//		    				writer.write("\t\t\tvar response"+i+" = clientEndpoint"+i+"->put(\"\","+d_payload+");\r\n");
-//		    			i++;
-//		    		}
-//					writer.write("\t\t}\n"
-//							+ "\t}\n"
-//							+ "};\n");
 		    		
 		    	}else { //mqtt y get		
 		    			 
@@ -364,36 +352,13 @@ public class Parser {
 		    				antecedentesGet.add(s);
 		    			}
 		    		}
-		    		
-		    		
-	    			ArrayList<String[]> condicionesAll = new ArrayList<String[]>();
-	    			
-	    			
-		    		writer.write(Parser.newsEndpoints(antecedentesGet.toArray(new String[0]), "antecedente"));
+		
+		    		writer.write(Parser.newsEndpoints(antecedentesAll, "antecedente"));
 
 		    		int i=1;
-//		    		for (String s: antecedentesAll) {  
-//		    			String[] d = s.split("\\s+");
-//		    			String d_type = d[0];
-//		    			String d_host = d[1];
-//		    			String d_condition = d[2];
-//		    			condicionesAll.add(d);
-//		    			if(d_type.equals("get"))
-//		    				writer.write("http:Client antecedente"+i+" = new (\""+d_host+"\");\r\n");
-//		    			i++;
-//					}
 
 			    	writer.write(Parser.newsEndpoints(consecuentesAll, "consecuente"));
 
-//		    		int j = 1;
-//		    		for (String s: consecuentesAll) {  
-//		    			String[] d = s.split("\\s+");
-//		    			String d_method = d[0];
-//		    			String d_host = d[1];
-//		    			String d_payload = d[2];
-//		    			writer.write("http:Client clientEndpoint"+j+" = new (\""+d_host+"\");\r\n");
-//		    		    j++;
-//		    		}
 
 		       	    i=1;
 		    		for (String s: antecedentesAll) {
@@ -405,63 +370,10 @@ public class Parser {
 					}
 		    		
 		    		
-		    		writer.write(Parser.getTimerConfiguration(1000));
-//		    	    writer.write("\ntask:Scheduler timer = new({\r\n"
-//		    	    		+ "    intervalInMillis: 1000,\r\n"
-//		    	    		+ "    initialDelayInMillis: 0\r\n"
-//		    	    		+ "    //noOfRecurrences: 10\r\n"
-//		    	    		+ "});\n\n"
-//		    	    		+"var attachResult = timer.attach(service1);\r\n"
-//		    	    		+ "var startResult = timer.start();\r\n"
-//		    	    		+ "\n\n");
+		    		writer.write(Parser.getTimerConfiguration(Integer.parseInt(antecedentesGet.get(0).split(" ")[3]),5));
 
 			       	writer.write(Parser.getMain(antecedentesMqtt.toArray(new String[0]), 0));		    		
 
-//	    			writer.write("\npublic function main() returns error? {\n");
-//	    			
-//		    	    i=1;
-//
-//		    		for (String s: antecedentesMqtt) {  
-//		    			String[] d = s.split("\\s+");
-//		    			String d_host = d[1];
-//		    			String d_topic = d[2];
-//		    			String d_condition = d[3];
-//
-//		    			writer.write("\tmqtt:Client c"+i+" = check new(\"tcp://"+d_host+"\", \"\");\r\n"
-//		    					+ "    check c"+i+"->connect();\r\n"
-//		    					+ "    Callback cb"+i+" = new;\r\n"
-//		    					+ "    check c"+i+"->subscribe(\""+d_topic+"\", cb"+i+");\r\n\n");
-//		    			
-//		    			i++;
-//					}
-
-//	    			writer.write("\twhile(true){\r\n"
-//	    					+ "        runtime:sleep(1000);\r\n"
-//	    					+ "    }\r\n"
-//	    					+ "    check c1->disconnect();\r\n"
-//	    					+ "}\n\n"
-//	    					+ "public type Callback object {\r\n"
-//	    					+ "    public function onMessage(mqtt:Message m)  {\n"
-//	    					+ "        var payload = checkpanic str:fromBytes(m.payload);\r\n"
-//	    					+ "        json jsonpayload = checkpanic payload.fromJsonString();\n");
-//
-//	    			i=1;
-//		    		for (String s: antecedentesMqtt) {  
-//		    			String[] d = s.split("\\s+");
-//		    			String d_host = d[1];
-//		    			String d_topic = d[2];
-//		    			String d_condition = d[3];
-//
-//		    			writer.write("\t\tif(m.topic == \""+d_topic+"\"){\r\n"
-//		    					+ "            if(jsonpayload."+d_condition+"){\r\n"
-//		    					+ "                fired"+i+" = true;\r\n"
-//		    					+ "            }else{\r\n"
-//		    					+ "                fired"+i+" = false;\r\n"
-//		    					+ "            }\r\n"
-//		    					+ "        }\n");
-//		    			
-//		    			i++;
-//					}
 		    		
 		    		String condicion_bal_mqtt_get = condicion;
 
@@ -474,100 +386,10 @@ public class Parser {
 		    			}
 		    		}
 		    		
-			    	writer.write(Parser.getCallback(antecedentesMqtt.toArray(new String[0]), consecuentesAll, condicion_bal_mqtt_get));
+			    	writer.write(Parser.getCallback(antecedentesMqtt.toArray(new String[0]), consecuentesAll, condicion_bal_mqtt_get, 5));
 
-//	    			writer.write("\n\t\tif("+condicion_bal_mqtt_get+"){\n");
-		    		
-		    		//SALIDAS
-//	    			i=1;
-//		    		for (String s: consecuentesAll) {  
-//		    			String[] d = s.split("\\s+");
-//		    			String d_method = d[0];
-//		    			String d_payload = d[2];
-//		    			if(d_method.equals("post"))
-//		    				writer.write("\t\t\t\tvar response"+i+" = clientEndpoint"+i+"->put(\"\","+d_payload+");\r\n");
-//		    			i++;
-//		    		}
+		    		writer.write(Parser.newService(antecedentesAll, consecuentesAll, condicion_bal_mqtt_get, lastNumberCondition, 5));
 
-	    			writer.write("\n\t\t}\n"
-	    					+"}\n"
-	    					+"};\n"
-	    					+ "service service1 = service {\r\n"
-	    					+ "    resource function onTrigger() {\r\n"
-	    					+ "");
-
-
-	    			i=1;
-		    		for (String s: antecedentesAll) {  
-		    			String[] d = s.split("\\s+");
-		    			String d_type = d[0];
-		    			String d_host = d[1];
-
-		    			String d_condition = d[2];
-		    			if(d_type.equals("get"))
-		    				writer.write("\t\tvar res"+i+" = antecedente"+i+"->get(\"\");\n");
-		    	    			
-		    			i++;
-					}
-		    			writer.write("\n\t\tif(");
-		    		i=1;
-		    		boolean escribo = false;
-		      		for (String s: antecedentesAll) {  
-		    			String[] d = s.split("\\s+");
-		    			String d_type = d[0];
-		    			String d_host = d[1];
-		    			String d_condition = d[2];
-		    			if(d_type.equals("get")) {
-			    			if(escribo==false) {
-				    			writer.write("res"+i+" is http:Response");
-				    			escribo=true;
-			    			}else
-				    			writer.write(" && res"+i+" is http:Response");
-		    			}
-		    			i++;
-					}
-	    			writer.write(") {\n");
-
-		      		i=1;
-		    		for (String s: antecedentesAll) {  
-		    			String[] d = s.split("\\s+");
-		    			String d_type = d[0];
-
-		    			String d_host = d[1];
-		    			String d_condition = d[2];
-		    			if(d_type.equals("get")) {
-		    				writer.write("\t\t\tjson jsonpayload"+i+" = checkpanic res"+i+".getJsonPayload();\n"+
-			    						"\t\t\tvar condition"+i+" = <float>jsonpayload"+i+"."+condicionesAll.get(i-1)[2]+";\n");
-		    				writer.write("            if(condition"+i+"){\r\n"
-		    						+ "                firedGet"+i+" = true;\r\n"
-		    						+ "            }else{\r\n"
-		    						+ "                firedGet"+i+" = false;\r\n"
-		    						+ "            }\r\n");
-		    			}
-		    			i++;
-					}
-		    		
-
-	    			writer.write("\n\t\tif("+condicion_bal_mqtt_get+"){\n");
-		    		
-		    		//SALIDAS
-	    			i=1;
-		    		for (String s: consecuentesAll) {  
-		    			String[] d = s.split("\\s+");
-		    			String d_method = d[0];
-		    			String d_payload = d[2];
-		    			if(d_method.equals("post"))
-		    				writer.write("\t\t\t\tvar response"+i+" = clientEndpoint"+i+"->put(\"\","+d_payload+");\r\n");
-		    			i++;
-		    		}
-
-	    			writer.write("\n\t\t}\n");
-	    	
-	    			
-	    			writer.write("        }\r\n"
-	    					+ "    }\r\n"
-	    					+ "};\r\n"
-	    					+ "");
 
 		    	}
 		    }else{ //Un solo antecedente
@@ -581,12 +403,12 @@ public class Parser {
 		    		
 			       	writer.write(Parser.getMain(antecedentesAll,0));		    		
 		    					    	
-			    	writer.write(Parser.getCallback(antecedentesAll, consecuentesAll, condicion));
+			    	writer.write(Parser.getCallback(antecedentesAll, consecuentesAll, condicion, 2));
 			    	
 		    	}else if (antecedentes.contains("get")) { // antecedente GET
 		    		
 		    		writer.write(Parser.getImport("ballerina/task"));
-		    		writer.write(Parser.getTimerConfiguration(Integer.parseInt(antecedentes.split(" ")[3])));
+		    		writer.write(Parser.getTimerConfiguration(Integer.parseInt(antecedentes.split(" ")[3]),1));
 		    		
 		    		writer.write(Parser.newsEndpoints(antecedentesAll, "antecedente"));
 		    		
@@ -594,7 +416,7 @@ public class Parser {
 			    	
 		      		writer.write(Parser.getMain(null, 1));
 
-		    		writer.write(Parser.newService(antecedentesAll, consecuentesAll, condicion, lastNumberCondition));
+		    		writer.write(Parser.newService(antecedentesAll, consecuentesAll, condicion, lastNumberCondition, 1));
 		    		   		
 		 	   	}
 		    }
